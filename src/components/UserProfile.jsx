29d1b91c62a0926913d195b8978e2df09d22aad4
allyavorsky <allyavorsky@proton.me>
2025-09-04 12:36:33 +0300

    HW43-4: consume context with useContext hook in UserDetails

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,13 +1,14 @@
 import UserDetails from "./UserDetails";
 
-const UserProfile = ({ user }) => {
+const UserProfile = () => {
   return (
     <div
       style={{ border: "1px solid orange", padding: "16px", marginTop: "16px" }}
     >
       <h2>Component UserProfile (Рівень 2)</h2>
-      <p>Проміжний компонент який не використовує данні.</p>
-      <UserDetails user={user} />
+      <p>Компонент більше не передає props, які йому не потрібні.</p>
+
+      <UserDetails />
     </div>
   );
 };
